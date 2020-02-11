9d888ff2e9a4bbfcbaafb619805808caf7036b7d
angeelgod <zubzero2000@gmail.com>
2020-02-11 09:25:34 -0300

    Finish draft for puzzle assignment finished toggleImage function

--- puzzle/script.js
+++ puzzle/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 	$.fn.extend({
-		toggleImage: function(a, b){
-			return $("img").attr($("img").attr("src") == b ? a : b);
+		toggleImage2: function(a, b){
+			return $(this).attr({"src":$(this).attr("src") == b ? a : b,"src-alt":$(this).attr("src-alt") == b ? a : b});
 		}
 	});
-	$("img").click(function(){
-		$(this).toggleImage("img/cat0.png", "img/ninja0.png");
+	$("img").click(function() {
+		$(this).toggleImage2($(this).attr("src"),$(this).attr("src-alt"));
 	});
 });
