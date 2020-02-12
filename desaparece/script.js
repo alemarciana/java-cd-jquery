d3477321ad50b2ac706bb5d5538f459e7c9271c2
angeelgod <zubzero2000@gmail.com>
2020-02-12 13:27:10 -0300

    Added random background gradient

--- desaparece/script.js
+++ desaparece/script.js
@@ -1,8 +1,20 @@
+function random_color()
+{
+	var rgb = ['a','b','c','d','e','f','0','1','2','3','4','5','6','7','8','9'];
+	color = '#'
+	for(var i = 0; i < 6; i++)
+	{
+		x = Math.floor((Math.random()*16));
+		color += rgb[x];
+	}
+	return color;
+}
 $(document).ready(function(){
 	$(".image").click(function(){
 		$(this).addClass("invisible");
 //		alert("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
 		Audio('scream.wav').play();
+		$("#container").css("background-image","linear-gradient("+random_color()+","+random_color()+","+random_color()+","+random_color()+")");
 	});
 	$(".reset").click(function(){
 		$(".image").removeClass("invisible");
